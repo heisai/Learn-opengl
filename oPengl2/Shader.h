cfc75d430d0f31629e2b72060cfb971506bdffac
heisai <1049870353@qq.com>
2022-08-01 00:32:33 +0800

    add redeme

--- oPengl2/Shader.h
+++ oPengl2/Shader.h
@@ -26,28 +26,29 @@ class BufferData
 public:
 	void BindBufferData()
 	{
+		// 顶点数据
 		float vertices[] = {
 		-0.5f, -0.5f, 0.0f, // left  
 		 0.5f, -0.5f, 0.0f, // right 
 		 0.0f,  0.5f, 0.0f  // top   
 		};
 
+		// 顶点数组对象
 		glGenVertexArrays(1, &VAO);
+		//顶点缓冲对象
 		glGenBuffers(1, &VBO);
-		// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+		//绑定VAO
 		glBindVertexArray(VAO);
-
+		//把顶点数组复制到缓冲中供OpenGL使用
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+		//设置顶点属性指针
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
 
-		// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+		//绑定缓冲对象
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-		// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 		glBindVertexArray(0);
 	}
 	unsigned int  GetVAO()
