41def1e387bbca670f9d1a68f7eaff8a1c205d5a
heisai <1049870353@qq.com>
2022-08-07 16:04:44 +0800

    添加自定义shader 类

--- opengl3/Shader.cpp
+++ opengl3/Shader.cpp
@@ -60,3 +60,72 @@ unsigned  int Shader::GetPromger()
 {
 	return ShaderPromger;
 }
+
+MyShader::MyShader(const std::string& vertexPath, const std::string& fragmentPath)
+{
+	vShaderCode = ReadShaderCode(vertexPath);
+	gShaderCode = ReadShaderCode(fragmentPath);
+	this->CreatShader();
+	this->BindShader();
+}
+
+void MyShader::use()
+{
+	glUseProgram(ShaderPromger);
+}
+
+void MyShader::CreatShader()
+{
+	//顶点着色器对象
+	vertecShader = glCreateShader(GL_VERTEX_SHADER);
+	//将源码添加到对象上
+	glShaderSource(vertecShader, 1, &vertexShaderSource, NULL);
+	//编译着色器对象
+	glCompileShader(vertecShader);
+
+	//片段着色器对象
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//将源码添加到对象上
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	//编译着色器对象
+	glCompileShader(fragmentShader);
+}
+
+void MyShader::BindShader()
+{
+	// 创建着色器程序
+	ShaderPromger = glCreateProgram();
+	// 将着色器对象添加到着色器程序上
+	glAttachShader(ShaderPromger, vertecShader);
+	glAttachShader(ShaderPromger, fragmentShader);
+	//编译着色器程序
+	glLinkProgram(ShaderPromger);
+
+	glDeleteShader(vertecShader);
+	glDeleteShader(fragmentShader);
+}
+
+const char* MyShader::ReadShaderCode(const std::string &filePath)
+{
+	std::string texCode;
+	std::ifstream ShaderFile;
+	ShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try
+	{
+		// open files
+		ShaderFile.open(filePath);
+		std::stringstream ShaderStream;
+		// read file's buffer contents into streams
+		ShaderStream << ShaderFile.rdbuf();
+		// close file handlers
+		ShaderFile.close();
+
+		// convert stream into string
+		texCode = ShaderStream.str();
+	}
+	catch (std::ifstream::failure& e)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ: " << e.what() << std::endl;
+	}
+	return texCode.c_str();
+}
--- opengl3/Shader.h
+++ opengl3/Shader.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 class Shader
 {
 public:
@@ -18,6 +21,50 @@ private:
 
 
 };
+
+
+/*
+自定义 Shader 类
+*/
+class MyShader
+{
+public:
+	// 构造器读取并构建着色器
+	MyShader(const std::string& vertexPath, const std::string& fragmentPath);
+	// 使用/激活程序
+	void use();
+protected:
+	// uniform工具函数
+	template<typename T>
+	void SetUniform1i(const std::string& name, const T& value)
+	{
+		if (std::is_same<int,T>::value)
+		{
+			glUniform1i(glGetUniformLocation(ShaderPromger, name.c_str()), (int)value);
+		}
+		else if (std::is_same<int, T>::value)
+		{
+			glUniform1i(glGetUniformLocation(ShaderPromger, name.c_str()), value);
+		}
+		else if (std::is_same<float, T>::value)
+		{
+			glUniform1f(glGetUniformLocation(ShaderPromger, name.c_str()), value);
+		}
+		
+	}
+	void CreatShader();
+	void BindShader();
+	const char* ReadShaderCode(const std::string &filePath);
+private:
+	const char* vShaderCode;
+	const char* gShaderCode;
+	unsigned int vertecShader;
+	unsigned int fragmentShader;
+	unsigned int ShaderPromger;
+
+};
+
+
 
 
 
